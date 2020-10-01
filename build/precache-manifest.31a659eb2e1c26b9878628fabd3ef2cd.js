485437ef86417c1b9d843a2d26529be847b1f456
numwebdev <72181738+numwebdev@users.noreply.github.com>
2020-10-01 09:53:55 +0530

    Adding Project Files
    Adding Project Files

---
+++ build/precache-manifest.31a659eb2e1c26b9878628fabd3ef2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb16772939c592a94bbe8d606ed4965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d65bca81c5c894602da",
+    "url": "/static/css/main.29e488e1.chunk.css"
+  },
+  {
+    "revision": "346ca2f6f6e040219dd4",
+    "url": "/static/js/2.0195dca6.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.0195dca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d65bca81c5c894602da",
+    "url": "/static/js/main.8e5a32de.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
